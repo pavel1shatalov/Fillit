af92a5739bd11ac646d386ff727e6fe7064b5711
Tysane frey Hansen <thansen@e2r7p3.21-school.ru>
2019-02-14 14:49:23 +0300

    thansen's branch

--- ft_sqrt.c
+++ ft_sqrt.c
@@ -1,24 +1,15 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_mupdate.c                                       :+:      :+:    :+:   */
+/*   ft_sqrt.c                                          :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: ggerhold <marvin@42.fr>                    +#+  +:+       +#+        */
+/*   By: thansen <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2019/02/11 21:43:30 by ggerhold          #+#    #+#             */
-/*   Updated: 2019/02/14 14:43:27 by thansen          ###   ########.fr       */
+/*   Created: 2019/02/14 14:44:53 by thansen           #+#    #+#             */
+/*   Updated: 2019/02/14 14:47:07 by thansen          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "fillit.h"
 
-char	*ft_mupdate(char *map)
-{
-	int		size;
-
-	size = 0;
-	while (map[size] != '\n')
-		size++;
-	ft_strdel(&map);
-	return (ft_map(size + 1));
-}
+int		ft_sqrt(int n);
